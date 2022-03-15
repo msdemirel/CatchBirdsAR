9631066e74bfdf9c28d755ef4bf40be7a41f2431
msdemirel <aa2996@student.jamk.fi>
2022-03-15 21:32:20 +0200

    Random Game Object spawn Done

--- Catch a Bird/Assets/Scripts/CollectHearts.cs
+++ Catch a Bird/Assets/Scripts/CollectHearts.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CollectHearts : MonoBehaviour
 {
     public GameObject Heart;
+   
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -15,26 +19,41 @@ public class CollectHearts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began) )
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            RaycastHit hit;
-            
 
-            if (Physics.Raycast(ray, out hit))
+            if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
             {
-                Debug.Log("Something hit");
-                if(hit.collider.CompareTag("Hearts"))
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hit;
+                
+
+
+                if (Physics.Raycast(ray, out hit))
                 {
-                    Debug.Log("Heart hit");
-                    Heart = hit.transform.gameObject;
-                    this.Heart.SetActive(false);
+                   
+
+                    if (hit.collider.CompareTag("Hearts"))
+                    {
+
+                        Heart = hit.transform.gameObject;
+                        
+                        ScoreManager.score += 1;
+                        SpawnHeart();
+                    Destroy(this.gameObject);
+
+
+
+
+
+                    }
                 }
             }
+        
+        
+    }
 
-            
-           // Destroy(Heart);
-        }
+    void SpawnHeart()
+    {
+        Vector3 position = new Vector3(Random.Range(-10.0F, 10.0F), 1, Random.Range(-10.0F, 10.0F));
+        Instantiate(Heart, position, transform.rotation);
     }
 }
